bff5d54cdf5e5bce31123666fe8de3dd91853b91
Luca <80696098+LucaLunati89@users.noreply.github.com>
2022-05-11 10:55:50 +0200

    feat: backend working👻

--- README.md
+++ README.md
@@ -2,3 +2,99 @@
 
 This is a simple fullstack random number generator developed during the 
 Lacerba course of Node and Typescript
+
+## Install
+
+```bash
+npm install
+```
+
+## Usage backend
+
+```bash
+ts-node main.ts
+```
+
+example output:
+
+```
+{
+  "Bari": [
+    60,
+    33,
+    24,
+    29,
+    86
+  ],
+  "Cagliari": [
+    94,
+    64,
+    58,
+    74,
+    92
+  ],
+  "Firenze": [
+    19,
+    9,
+    53,
+    21,
+    36
+  ],
+  "Genova": [
+    73,
+    12,
+    29,
+    89,
+    44
+  ],
+  "Milano": [
+    27,
+    7,
+    52,
+    77,
+    18
+  ],
+  "Napoli": [
+    23,
+    62,
+    93,
+    15,
+    46
+  ],
+  "Palermo": [
+    40,
+    63,
+    61,
+    56,
+    41
+  ],
+  "Roma": [
+    55,
+    19,
+    8,
+    46,
+    21
+  ],
+  "Torino": [
+    7,
+    97,
+    33,
+    59,
+    56
+  ],
+  "Venezia": [
+    54,
+    86,
+    8,
+    19,
+    56
+  ],
+  "Nazionale": [
+    35,
+    70,
+    58,
+    5,
+    6
+  ]
+}
+```
--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-console.log('hello world!');
+function randomNumberGenerator (min : number, max : number) : number {
+  const initialRandomNumber = Math.random();
+  return Math.trunc(initialRandomNumber * (max - min) + min);
+}
+
+function randomNumberGeneratorSeq(len : number, min : number, max : number) : number[]{
+  if (len > (max - min)){
+    throw new Error(`cannot find ${len} numbers between  ${min} and ${max}`);
+  }
+  const res : number[] = [];
+  while(res.length < len){
+    const rn = randomNumberGenerator(min, max);
+    if(!res.includes(rn)){
+      res.push(rn);
+    }
+  }
+return res;
+}
+
+const ruote : string[] = ["Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino", "Venezia", "Nazionale"];
+
+const estrazioni : {[ruota : string] : number[]} = {};
+
+for (const ruota of ruote) {
+  const estrazione = randomNumberGeneratorSeq(5, 1, 100);
+  estrazioni [ruota] = estrazione;
+}
+console.log(JSON.stringify(estrazioni, null, 2));
